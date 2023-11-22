d6415209757e03710f9ed85cb9702ea036553a2a
baisreycarlo <bais.reycarlo@gmail.com>
2023-11-22 23:57:38 +0800

    first commit

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+// const express = require('express');
+// const cors = require('cors');
+// const axios = require('axios');
 
-const app = express();
-const port = 3001;
+// const app = express();
+// const port = 3001;
 
-app.use(cors());//test
+// app.use(cors());//test
 
-app.get('/api/restaurants', async (req, res) => {
-  try {
-    const response = await axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants');
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// app.get('/api/restaurants', async (req, res) => {
+//   try {
+//     const response = await axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants');
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
