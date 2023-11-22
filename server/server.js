2c112a598285de9fe9f0a8b45736e52d7e5d0009
baisreycarlo <bais.reycarlo@gmail.com>
2023-11-22 23:34:38 +0800

    vercel.json

--- server/server.js
+++ server/server.js
@@ -5,17 +5,17 @@ const axios = require('axios');
 const app = express();
 const port = 3001;
 
-// app.use(cors());//test
+app.use(cors());//test
 
-// app.get('/api/restaurants', async (req, res) => {
-//   try {
-//     const response = await axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants');
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
+app.get('/api/restaurants', async (req, res) => {
+  try {
+    const response = await axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
