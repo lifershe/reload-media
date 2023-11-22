fc2e38f01a5254a87545f9689d43bc4017d59db7
baisreycarlo <bais.reycarlo@gmail.com>
2023-11-22 23:59:15 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.use("/", (req, res) => {
-    res.send("server is running");
+// app.use("/", (req, res) => {
+//     res.send("server is running");
+// });
+
+// app.listen(5000, console.log("Server started on PORT 5000"));
+
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());//test
+
+app.get('/api/restaurants', async (req, res) => {
+  try {
+    const response = await axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
