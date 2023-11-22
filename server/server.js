3a66a4827a2417ec9999b8a984fef264695508db
baisreycarlo <bais.reycarlo@gmail.com>
2023-11-22 23:42:29 +0800

    vercel.json

--- server/server.js
+++ server/server.js
@@ -1,22 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const http = require('http');
 
-const app = express();
-const port = 3001;
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
 
-app.use(cors());//test
-
-app.get('/api/restaurants', async (req, res) => {
-  try {
-    const response = await axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants');
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+  // Send a response
+  res.end('Hello, World!\n');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const port = process.env.PORT || 3000;
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
 });
